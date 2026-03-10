3cd21657540b9cb98f7b41b274c00d26cab883c9
674259021Wilasinee <674259021@webmail.npru>
2026-03-10 15:01:17 +0700

    inputAndIf

--- inputAndIf.java
+++ inputAndIf.java
@@ -1,3 +1,34 @@
-public class inputAndIf {
+import java.util.Scanner;
 
+public class inputAndIf {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter gender (M/F): ");
+        String gender = input.next();
+
+        System.out.print("Enter age: ");
+        int age = input.nextInt();
+
+        System.out.print("Enter year: ");
+        int year = input.nextInt();
+
+        if (age >= 18) {
+            if (gender.equalsIgnoreCase("M") && age >= 21) {
+                System.out.println("You can vote and serve nation");
+            } else {
+                System.out.println("You can vote");
+            }
+        } else {
+            System.out.println("You can not vote");
+        }
+
+        System.out.println("Year: " + year);
+
+        input.close();
+    }
 }
